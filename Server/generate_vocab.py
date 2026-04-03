ab7b3786d763a422e5259f072f840341c59c9849
一只一只 <117522929+123-wang-en-ci@users.noreply.github.com>
2026-04-03 10:51:01 +0800

    Add files via upload

---
+++ Server/generate_vocab.py
@@ -0,0 +1,16 @@
+import scanpy as sc
+import numpy as np
+import os
+
+H5AD_FILE = "Allen2022Molecular_lps_MsBrainAgingSpatialDonor_14_1.h5ad"
+
+if not os.path.exists(H5AD_FILE):
+    print("not found train.h5ad")
+    exit()
+
+print(f"Reading {H5AD_FILE}...")
+adata = sc.read_h5ad(H5AD_FILE)
+genes = adata.var_names.tolist()
+
+np.save("gene_vocab.npy", genes)
+print(f"saved gene_vocab.npy (contains {len(genes)} genes)")
